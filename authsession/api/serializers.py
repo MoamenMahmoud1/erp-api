b62c9daaa28f44d7dc7157df5fe4773b9c634c2c
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-03 22:33:21 -0400

    refactor(authsession): remove remaining async serializer code

--- authsession/api/serializers.py
+++ authsession/api/serializers.py
@@ -1,5 +1,4 @@
-from adrf import serializers
-from asgiref.sync import sync_to_async
+from rest_framework import serializers
 
 from authsession.http import get_device_id
 from authsession.models import AuthSession
@@ -22,21 +21,12 @@ class AuthSessionSerializer(serializers.ModelSerializer):
         )
         read_only_fields = fields
 
-    async def get_is_current(self, auth_session) -> bool:
+    def get_is_current(self, auth_session) -> bool:
         request = self.context.get("request")
         return bool(
             request
             and get_device_id(request) == auth_session.device_id
         )
-
-    async def acreate(self, validated_data):
-        return await sync_to_async(self.create, thread_sensitive=True)(validated_data)
-
-    async def aupdate(self, instance, validated_data):
-        return await sync_to_async(
-            self.update,
-            thread_sensitive=True,
-        )(instance, validated_data)
 
 
 class AuthSessionVerificationSerializer(serializers.Serializer):
