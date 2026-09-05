d6e203b4ad0bd29dc86dace73d2572f0d57d07cd
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-04 23:40:36 -0400

    test(invoices): remove unused scope test import

--- invoices/tests/test_scope.py
+++ invoices/tests/test_scope.py
@@ -1,9 +1,7 @@
-from decimal import Decimal
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from invoices.models import Invoice, InvoiceItem
+from invoices.models import Invoice
 from .helpers import InvoiceTestMixin
 
 
