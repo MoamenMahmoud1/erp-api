942b637de4ac4bed0a5c11c3b47996515fd8261c
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-05 02:15:13 -0400

    test(p2): cover inventory integrity constraints

--- inventory/tests/test_models.py
+++ inventory/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
 from inventory.models import StockLocation, StockMovement, StockMovementItem
@@ -8,7 +9,10 @@ from .helpers import InventoryTestMixin
 class InventoryModelTests(InventoryTestMixin, TestCase):
     def test_vehicle_belongs_to_employee(self):
         self.assertEqual(self.vehicle.employee, self.user)
-        self.assertEqual(self.vehicle.location_type, StockLocation.LocationType.SALES_VEHICLE)
+        self.assertEqual(
+            self.vehicle.location_type,
+            StockLocation.LocationType.SALES_VEHICLE,
+        )
 
     def test_stock_movement_can_record_multiple_items(self):
         movement = StockMovement.objects.create(
@@ -16,5 +20,26 @@ class InventoryModelTests(InventoryTestMixin, TestCase):
             destination_location=self.warehouse,
             created_by=self.user,
         )
-        StockMovementItem.objects.create(movement=movement, product=self.product, quantity=10)
+        StockMovementItem.objects.create(
+            movement=movement,
+            product=self.product,
+            quantity=10,
+        )
         self.assertEqual(movement.items.count(), 1)
+
+    def test_movement_direction_must_match_type(self):
+        with self.assertRaises(IntegrityError):
+            StockMovement.objects.create(
+                movement_type=StockMovement.MovementType.PURCHASE,
+                source_location=self.warehouse,
+                created_by=self.user,
+            )
+
+    def test_transfer_requires_two_distinct_locations(self):
+        with self.assertRaises(IntegrityError):
+            StockMovement.objects.create(
+                movement_type=StockMovement.MovementType.TRANSFER,
+                source_location=self.warehouse,
+                destination_location=self.warehouse,
+                created_by=self.user,
+            )
