b83bdd7ccb06bb43f518e0088c0c888472c3f3b2
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-04 23:25:34 -0400

    refactor(purchases): use shared organization visibility queryset

--- purchases/querysets/purchase.py
+++ purchases/querysets/purchase.py
@@ -1,17 +1,11 @@
 from django.db import models
 
+from services.organization_scope import filter_by_actor_scope
+
 
 class PurchaseQuerySet(models.QuerySet):
     def with_purchase_data(self):
-        return (
-            self.select_related(
-                "supplier",
-                "created_by",
-            )
-            .prefetch_related(
-                "items__product",
-            )
-        )
+        return self.select_related("supplier", "created_by").prefetch_related("items__product")
 
     def visible_to(self, user):
-        return self.filter(created_by=user)
+        return filter_by_actor_scope(self, user=user, owner_field="created_by_id")
