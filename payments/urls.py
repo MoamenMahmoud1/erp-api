e3a53fcccda412c82efb39fd12c8af631e65ada5
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-04 23:24:27 -0400

    feat(payments): expose refund endpoint

--- payments/urls.py
+++ payments/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path
 
-from payments.api.views import CollectionView, TransactionListView
+from payments.api.views import CollectionView, RefundView, TransactionListView
 
 app_name = "payments"
 
 urlpatterns = [
-    path(
-        "collections/",
-        CollectionView.as_view(),
-        name="payment-collection",
-    ),
-    path(
-        "transactions/",
-        TransactionListView.as_view(),
-        name="payment-transaction-list",
-    ),
+    path("collections/", CollectionView.as_view(), name="payment-collection"),
+    path("refunds/", RefundView.as_view(), name="payment-refund"),
+    path("transactions/", TransactionListView.as_view(), name="payment-transaction-list"),
 ]
