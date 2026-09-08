c2e8b2b211201ad3e7e4e0b1d2ed0e3d34b63c16
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-07 20:47:14 -0400

    chore(admin): style accounting admin with unfold

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 
+from unfold.admin import ModelAdmin
+
 from accounting.models import Account, JournalEntry, JournalLine
 
 
 @admin.register(Account)
-class AccountAdmin(admin.ModelAdmin):
+class AccountAdmin(ModelAdmin):
     list_display = ("code", "name", "account_type", "company", "is_active")
     list_filter = ("account_type", "is_active")
     search_fields = ("code", "name")
+    ordering = ("code",)
+    list_select_related = ("company",)
 
 
 class JournalLineInline(admin.TabularInline):
@@ -16,9 +20,11 @@ class JournalLineInline(admin.TabularInline):
 
 
 @admin.register(JournalEntry)
-class JournalEntryAdmin(admin.ModelAdmin):
+class JournalEntryAdmin(ModelAdmin):
     list_display = ("number", "entry_date", "description", "status", "company")
     list_filter = ("status", "entry_date")
     search_fields = ("description", "reference")
+    ordering = ("-entry_date", "-number")
+    list_select_related = ("company", "created_by", "posted_by")
     readonly_fields = ("number", "posted_at", "posted_by", "created_by")
     inlines = (JournalLineInline,)
