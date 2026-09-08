794de0e542812f1a99c8dbc766d0362fe4128c58
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-07 21:01:33 -0400

    chore(admin): restore standard products admin

--- products/admin.py
+++ products/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
-from unfold.admin import ModelAdmin
-
 from .models import CartonPricing, Product
 
 
 @admin.register(Product)
-class ProductAdmin(ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "category", "purchase_price", "selling_price", "created_at")
     search_fields = ("name", "category")
     list_filter = ("category",)
@@ -14,7 +12,7 @@ class ProductAdmin(ModelAdmin):
 
 
 @admin.register(CartonPricing)
-class CartonPricingAdmin(ModelAdmin):
+class CartonPricingAdmin(admin.ModelAdmin):
     list_display = ("name", "units_per_carton", "carton_price")
     search_fields = ("name",)
     ordering = ("name",)
