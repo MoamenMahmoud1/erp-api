2815cb9634980913ee596b3046a453d9ef2b979b
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-07 21:01:29 -0400

    chore(admin): restore standard customers admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
-from unfold.admin import ModelAdmin
-
 from .models import Customer
 
 
 @admin.register(Customer)
-class CustomerAdmin(ModelAdmin):
+class CustomerAdmin(admin.ModelAdmin):
     list_display = ("name", "phone", "created_at")
     search_fields = ("name", "phone")
     ordering = ("name",)
