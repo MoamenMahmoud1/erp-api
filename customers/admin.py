2eab3353d84cc9b40b3a8e7bdf16e24547dc979f
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-07 20:47:21 -0400

    chore(admin): style customer admin with unfold

--- customers/admin.py
+++ customers/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
+from unfold.admin import ModelAdmin
+
 from .models import Customer
 
 
 @admin.register(Customer)
-class CustomerAdmin(admin.ModelAdmin):
+class CustomerAdmin(ModelAdmin):
     list_display = ("name", "phone", "created_at")
     search_fields = ("name", "phone")
+    ordering = ("name",)
