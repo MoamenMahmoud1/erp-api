d750202be2ec6f1fd645b2320da8528e796d6d02
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 00:57:19 -0400

    Add employee option endpoint permission

--- accounts/permissions/employee.py
+++ accounts/permissions/employee.py
@@ -9,6 +9,8 @@ class EmployeeAccessPermission(BasePermission):
         "alist": "accounts.view_employee",
         "retrieve": "accounts.view_employee",
         "aretrieve": "accounts.view_employee",
+        "options": "accounts.view_employee",
+        "aoptions": "accounts.view_employee",
         "create": "accounts.add_employee",
         "acreate": "accounts.add_employee",
         "update": "accounts.change_employee",
