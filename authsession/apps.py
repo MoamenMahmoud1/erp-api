b2d1576df60de5c995fc1a5d674e46fa9c255ac3
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-07 23:47:14 -0400

    security: load permission invalidation signals

--- authsession/apps.py
+++ authsession/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AuthSessionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "authsession"
+
+    def ready(self):
+        from . import signals  # noqa: F401
