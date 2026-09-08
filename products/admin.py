412a568be1b477a0af5e9039144e1508aa94b7cd
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-07 20:47:31 -0400

    chore(admin): style product admin with unfold

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 
+from unfold.admin import ModelAdmin
+
 from .models import CartonPricing, Product
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ModelAdmin):
     list_display = ("name", "category", "purchase_price", "selling_price", "created_at")
     search_fields = ("name", "category")
     list_filter = ("category",)
+    ordering = ("name",)
 
 
 @admin.register(CartonPricing)
-class CartonPricingAdmin(admin.ModelAdmin):
+class CartonPricingAdmin(ModelAdmin):
     list_display = ("name", "units_per_carton", "carton_price")
     search_fields = ("name",)
+    ordering = ("name",)
