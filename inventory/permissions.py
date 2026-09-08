6b4e9bb97cef3f8976d9f728285bf1258b8fb488
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-07 21:36:41 -0400

    fix(authz): require explicit inventory permissions

---
+++ inventory/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import BasePermission
+
+
+class InventoryReadPermission(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        return bool(
+            user
+            and user.is_authenticated
+            and (user.is_superuser or user.has_perm("inventory.view_stockmovement"))
+        )
+
+
+class InventoryTransferPermission(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        return bool(
+            user
+            and user.is_authenticated
+            and (user.is_superuser or user.has_perm("inventory.transfer_stock"))
+        )
