79324bc5d4c9d14fbde418fe1350855b7e2356b8
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 22:42:05 -0400

    Apply site scope to payment visibility

--- payments/querysets.py
+++ payments/querysets.py
@@ -5,7 +5,7 @@ from services.organization_scope import filter_by_actor_scope
 
 class PaymentTransactionQuerySet(models.QuerySet):
     def visible_to(self, user):
-        return filter_by_actor_scope(self, user=user, owner_field="collected_by_id")
+        return filter_by_actor_scope(self, user=user, owner_field="collected_by_id", site_field="site_id")
 
     def with_payment_data(self):
-        return self.select_related("customer", "collected_by").prefetch_related("allocations__invoice", "refunds")
+        return self.select_related("customer", "collected_by", "site", "shift").prefetch_related("allocations__invoice", "refunds")
