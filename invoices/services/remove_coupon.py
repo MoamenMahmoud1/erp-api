d5ea3f99bb77da132b0e4e0c5fb6741507f53917
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 22:48:14 -0400

    Enforce shift context for removing invoice coupons

--- invoices/services/remove_coupon.py
+++ invoices/services/remove_coupon.py
@@ -1,6 +1,7 @@
 from django.db import transaction
 
-from common.exceptions import InvalidStateTransition
+from accounts.services.employee_shift import require_open_shift
+from common.exceptions import InvalidBusinessOperation, InvalidStateTransition
 
 from .lifecycle import load_invoice_for_update
 
@@ -10,6 +11,9 @@ def remove_coupon(invoice_id, actor=None):
     invoice = load_invoice_for_update(invoice_id, actor)
     if invoice.status != invoice.Status.DRAFT:
         raise InvalidStateTransition("A coupon can only be removed from a draft invoice.")
+    shift = require_open_shift(actor) if actor is not None else None
+    if shift is not None and invoice.site_id != shift.site_id:
+        raise InvalidBusinessOperation("The invoice belongs to a different site than the current shift.")
     invoice.coupon = None
     invoice.coupon_discount = 0
     invoice.save(update_fields=("coupon", "coupon_discount", "updated_at"))
