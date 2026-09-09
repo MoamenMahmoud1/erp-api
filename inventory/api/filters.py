d53cf6938595a5a070be330c65a1efe5e04b1a63
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 23:12:45 -0400

    Add batch expiry filters to inventory API

--- inventory/api/filters.py
+++ inventory/api/filters.py
@@ -1,6 +1,7 @@
 import django_filters
+from django.utils import timezone
 
-from inventory.models import StockMovement
+from inventory.models import StockBatchBalance, StockMovement
 
 
 class StockBalanceFilter(django_filters.FilterSet):
@@ -12,6 +13,26 @@ class StockBalanceFilter(django_filters.FilterSet):
         from inventory.models import StockBalance
         model = StockBalance
         fields = ("product", "location", "min_quantity")
+
+
+class StockBatchBalanceFilter(django_filters.FilterSet):
+    product = django_filters.NumberFilter(field_name="batch__product_id")
+    location = django_filters.NumberFilter(field_name="location_id")
+    min_quantity = django_filters.NumberFilter(field_name="quantity", lookup_expr="gte")
+    expiry_before = django_filters.DateFilter(field_name="batch__expiry_date", lookup_expr="lte")
+    expiry_after = django_filters.DateFilter(field_name="batch__expiry_date", lookup_expr="gte")
+    expired = django_filters.BooleanFilter(method="filter_expired")
+
+    class Meta:
+        model = StockBatchBalance
+        fields = ("product", "location", "min_quantity", "expiry_before", "expiry_after", "expired")
+
+    def filter_expired(self, queryset, name, value):
+        if value is None:
+            return queryset
+        today = timezone.localdate()
+        lookup = {"batch__expiry_date__lt": today} if value else {"batch__expiry_date__gte": today}
+        return queryset.filter(**lookup)
 
 
 class StockMovementFilter(django_filters.FilterSet):
