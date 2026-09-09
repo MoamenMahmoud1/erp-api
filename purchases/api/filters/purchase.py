7f90a5f6684caf2769fc7d588a6b15dabe62768b
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 22:09:47 -0400

    Add branch filter to purchases

--- purchases/api/filters/purchase.py
+++ purchases/api/filters/purchase.py
@@ -4,29 +4,12 @@ from purchases.models import Purchase
 
 
 class PurchaseFilter(django_filters.FilterSet):
-    status = django_filters.CharFilter(
-        field_name="status",
-    )
-
-    supplier = django_filters.NumberFilter(
-        field_name="supplier_id",
-    )
-
-    created_at_after = django_filters.IsoDateTimeFilter(
-        field_name="created_at",
-        lookup_expr="gte",
-    )
-
-    created_at_before = django_filters.IsoDateTimeFilter(
-        field_name="created_at",
-        lookup_expr="lte",
-    )
+    status = django_filters.CharFilter(field_name="status")
+    supplier = django_filters.NumberFilter(field_name="supplier_id")
+    site = django_filters.NumberFilter(field_name="site_id")
+    created_at_after = django_filters.IsoDateTimeFilter(field_name="created_at", lookup_expr="gte")
+    created_at_before = django_filters.IsoDateTimeFilter(field_name="created_at", lookup_expr="lte")
 
     class Meta:
         model = Purchase
-        fields = (
-            "status",
-            "supplier",
-            "created_at_after",
-            "created_at_before",
-        )
+        fields = ("status", "supplier", "site", "created_at_after", "created_at_before")
