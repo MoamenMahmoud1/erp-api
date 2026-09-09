b93aa368426bc29d7791c9121c91ad0260bf6731
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 22:45:08 -0400

    Include shift context in invoice queries

--- invoices/querysets.py
+++ invoices/querysets.py
@@ -5,7 +5,7 @@ from services.organization_scope import filter_by_actor_scope
 
 class InvoiceQuerySet(models.QuerySet):
     def with_invoice_data(self):
-        return self.select_related("customer", "created_by", "coupon", "site").prefetch_related("items__product")
+        return self.select_related("customer", "created_by", "coupon", "site", "shift").prefetch_related("items__product")
 
     def visible_to(self, user):
         return filter_by_actor_scope(self, user=user, owner_field="created_by_id", site_field="site_id")
