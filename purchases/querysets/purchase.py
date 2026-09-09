56da41579162ac9f2ae3b231186860bc4b5077f9
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 22:45:15 -0400

    Include purchase shift context in queries

--- purchases/querysets/purchase.py
+++ purchases/querysets/purchase.py
@@ -5,7 +5,7 @@ from services.organization_scope import filter_by_actor_scope
 
 class PurchaseQuerySet(models.QuerySet):
     def with_purchase_data(self):
-        return self.select_related("supplier", "created_by", "site").prefetch_related("items__product")
+        return self.select_related("supplier", "created_by", "site", "shift").prefetch_related("items__product")
 
     def visible_to(self, user):
         return filter_by_actor_scope(self, user=user, owner_field="created_by_id", site_field="site_id")
