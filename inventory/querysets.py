06f45065933ac7a896288428d7b517102026a621
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 22:39:19 -0400

    Handle company-wide inventory scope correctly

--- inventory/querysets.py
+++ inventory/querysets.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models import Q, Subquery
 
-from services.organization_scope import visible_employee_user_ids
+from services.organization_scope import visible_employee_user_ids, visible_site_ids
 
 
 class StockLocationQuerySet(models.QuerySet):
@@ -11,13 +11,14 @@ class StockLocationQuerySet(models.QuerySet):
         if user.is_superuser:
             return self
 
-        employee_ids = Subquery(visible_employee_user_ids(user))
         employee = getattr(user, "employee", None)
         if employee and employee.work_site_id:
-            from services.organization_scope import visible_site_ids
             site_ids = visible_site_ids(user)
+            if site_ids is None:
+                return self
             return self.filter(site_id__in=site_ids)
 
+        employee_ids = Subquery(visible_employee_user_ids(user))
         return self.filter(Q(employee_id=user.pk) | Q(employee_id__in=employee_ids))
 
     def active(self):
@@ -31,16 +32,18 @@ class StockMovementQuerySet(models.QuerySet):
         if user.is_superuser:
             return self
 
-        employee_ids = Subquery(visible_employee_user_ids(user))
         employee = getattr(user, "employee", None)
         if employee and employee.work_site_id:
-            from services.organization_scope import visible_site_ids
             site_ids = visible_site_ids(user)
+            if site_ids is None:
+                return self
             return self.filter(
                 Q(source_location__site_id__in=site_ids)
                 | Q(destination_location__site_id__in=site_ids)
+                | Q(shift__site_id__in=site_ids)
             ).distinct()
 
+        employee_ids = Subquery(visible_employee_user_ids(user))
         return self.filter(
             Q(created_by_id=user.pk)
             | Q(created_by_id__in=employee_ids)
