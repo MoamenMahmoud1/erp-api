43bcbce78add780179e0d931d96ab1d2e2f15112
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 22:08:19 -0400

    Scope invoice visibility to employee site

--- invoices/querysets.py
+++ invoices/querysets.py
@@ -5,10 +5,10 @@ from services.organization_scope import filter_by_actor_scope
 
 class InvoiceQuerySet(models.QuerySet):
     def with_invoice_data(self):
-        return self.select_related("customer", "created_by", "coupon").prefetch_related("items__product")
+        return self.select_related("customer", "created_by", "coupon", "site").prefetch_related("items__product")
 
     def visible_to(self, user):
-        return filter_by_actor_scope(self, user=user, owner_field="created_by_id")
+        return filter_by_actor_scope(self, user=user, owner_field="created_by_id", site_field="site_id")
 
     def drafts(self):
         from invoices.models import Invoice
