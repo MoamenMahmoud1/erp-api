8ef35b8ccdce2517afda62642713f2bb6ce4eed2
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 22:12:15 -0400

    Scope site choices to employee branch visibility

--- organization/api/views/site.py
+++ organization/api/views/site.py
@@ -4,26 +4,25 @@ from rest_framework import viewsets
 from common.permissions import ModelAccessPermission
 from organization.api.serializers import SiteSerializer
 from organization.models import Company, Site
+from services.organization_scope import visible_site_ids
 
 
 class SiteViewSet(viewsets.ModelViewSet):
-    queryset = Site.objects.select_related(
-        "company",
-        "parent",
-    )
+    queryset = Site.objects.select_related("company", "parent")
     serializer_class = SiteSerializer
     permission_classes = (ModelAccessPermission,)
-    http_method_names = (
-        "get",
-        "post",
-        "patch",
-        "head",
-        "options",
-    )
+    http_method_names = ("get", "post", "patch", "head", "options")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user = self.request.user
+        if user.is_superuser:
+            return queryset
+        site_ids = visible_site_ids(user)
+        if site_ids is None:
+            return queryset
+        return queryset.filter(pk__in=site_ids)
 
     def perform_create(self, serializer):
-        company = get_object_or_404(
-            Company.objects.all(),
-            singleton_marker=True,
-        )
+        company = get_object_or_404(Company.objects.all(), singleton_marker=True)
         serializer.save(company=company)
