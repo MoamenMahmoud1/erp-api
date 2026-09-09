46d50c53e4b1c2ea5f19c4c7c1a8152d68386bf4
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 21:47:30 -0400

    Register invoice counter signals

--- invoices/apps.py
+++ invoices/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class InvoicesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "invoices"
+
+    def ready(self):
+        from . import signals  # noqa: F401
