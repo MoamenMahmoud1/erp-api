456faddb6c7121e5ca821f4cb9f691b4ac9d67b4
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-08 22:08:29 -0400

    Make inventory visibility site-aware

--- inventory/querysets.py
+++ inventory/querysets.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models import Q, Subquery
 
-from services.organization_scope import visible_employee_user_ids
+from services.organization_scope import filter_by_actor_scope, visible_employee_user_ids
 
 
 class StockLocationQuerySet(models.QuerySet):
@@ -10,12 +10,18 @@ class StockLocationQuerySet(models.QuerySet):
             return self.none()
         if user.is_superuser:
             return self
+
         employee_ids = Subquery(visible_employee_user_ids(user))
-        return self.filter(
-            Q(location_type="MAIN_WAREHOUSE")
-            | Q(employee_id=user.pk)
+        queryset = self.filter(
+            Q(employee_id=user.pk)
             | Q(employee_id__in=employee_ids)
         )
+        employee = getattr(user, "employee", None)
+        if employee and employee.work_site_id:
+            from services.organization_scope import visible_site_ids
+            site_ids = visible_site_ids(user)
+            queryset = queryset.filter(site_id__in=site_ids)
+        return queryset
 
     def active(self):
         return self.filter(is_active=True)
@@ -27,14 +33,23 @@ class StockMovementQuerySet(models.QuerySet):
             return self.none()
         if user.is_superuser:
             return self
+
         employee_ids = Subquery(visible_employee_user_ids(user))
-        return self.filter(
+        queryset = self.filter(
             Q(created_by_id=user.pk)
             | Q(created_by_id__in=employee_ids)
             | Q(source_location__employee_id=user.pk)
             | Q(destination_location__employee_id=user.pk)
             | Q(source_location__employee_id__in=employee_ids)
             | Q(destination_location__employee_id__in=employee_ids)
-            | Q(source_location__location_type="MAIN_WAREHOUSE")
-            | Q(destination_location__location_type="MAIN_WAREHOUSE")
         ).distinct()
+
+        employee = getattr(user, "employee", None)
+        if employee and employee.work_site_id:
+            from services.organization_scope import visible_site_ids
+            site_ids = visible_site_ids(user)
+            queryset = queryset.filter(
+                Q(source_location__site_id__in=site_ids)
+                | Q(destination_location__site_id__in=site_ids)
+            ).distinct()
+        return queryset
