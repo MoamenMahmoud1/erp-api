43266a72c9a8b5652cd243449daccf9c6bc3dceb
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-09 23:17:30 -0400

    fix(auth): rename GroupPolicy references to RoleProfile

--- authsession/signals.py
+++ authsession/signals.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import Group
 from django.db.models.signals import m2m_changed, post_delete, post_save
 from django.dispatch import receiver
 
-from accounts.models import GroupPolicy
+from accounts.models import RoleProfile
 from authsession.cache import delete_all_auth_session_caches, delete_auth_session_caches
 from authsession.models import AuthSession
 
@@ -70,11 +70,11 @@ def invalidate_deleted_group_session_caches(sender, instance, **kwargs):
     delete_all_auth_session_caches()
 
 
-@receiver(post_save, sender=GroupPolicy)
-def invalidate_group_policy_cache(sender, instance, **kwargs):
+@receiver(post_save, sender=RoleProfile)
+def invalidate_role_profile_cache(sender, instance, **kwargs):
     _invalidate_group_users(instance.group_id)
 
 
-@receiver(post_delete, sender=GroupPolicy)
-def invalidate_deleted_group_policy_cache(sender, instance, **kwargs):
+@receiver(post_delete, sender=RoleProfile)
+def invalidate_deleted_role_profile_cache(sender, instance, **kwargs):
     _invalidate_group_users(instance.group_id)
