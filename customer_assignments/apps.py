c20f732a70802b45ad3cbffbbcedce0fa4a9123c
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-10 22:13:11 -0400

    feat(customer-access): add assignment app

---
+++ customer_assignments/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CustomerAssignmentsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "customer_assignments"
+    verbose_name = "Customer assignments"
