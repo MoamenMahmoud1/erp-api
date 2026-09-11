1f965c26ee6728010fa58a84bd9a03c0767b2c8e
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-10 20:39:56 -0400

    feat(notifications): add notification serializer

---
+++ notifications/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from .models import Notification
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    is_read = serializers.BooleanField(read_only=True)
+
+    class Meta:
+        model = Notification
+        fields = (
+            "id",
+            "notification_type",
+            "title",
+            "body",
+            "target_type",
+            "target_id",
+            "data",
+            "is_read",
+            "read_at",
+            "created_at",
+        )
+        read_only_fields = fields
