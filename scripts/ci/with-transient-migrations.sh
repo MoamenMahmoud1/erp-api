d02d3cdd37ba67baf3fea545bda338e2db4d6ac3
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-23 18:36:34 -0400

    ci: make authsession migrations transient only

--- scripts/ci/with-transient-migrations.sh
+++ scripts/ci/with-transient-migrations.sh
@@ -2,10 +2,12 @@
 set -eu
 
 cleanup() {
-  find . -type f -path '*/migrations/[0-9]*.py' -delete
-  find . -type f -path '*/migrations/[0-9]*.pyc' -delete
+  rm -f authsession/migrations/[0-9]*.py
+  rm -f authsession/migrations/[0-9]*.pyc
+  rm -rf authsession/migrations/__pycache__
+  rmdir authsession/migrations 2>/dev/null || true
 }
 trap cleanup EXIT HUP INT TERM
 
-python manage.py makemigrations --noinput
+python manage.py makemigrations authsession --noinput
 "$@"
