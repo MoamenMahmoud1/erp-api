f21aafa8e5abd9edcec68c5289a204aa1440a343
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-23 19:03:36 -0400

    style: fix Ruff imports in hardened files

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from .views import health_live, health_ready, version_view
