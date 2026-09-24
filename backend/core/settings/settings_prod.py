39428c9f24993b60e7db091e518f57226ad6421c
Moamen <161596823+MoamenMahmoud1@users.noreply.github.com>
2026-09-24 15:31:11 -0400

    Use WhiteNoise compressed manifest storage in production

--- backend/core/settings/settings_prod.py
+++ backend/core/settings/settings_prod.py
@@ -77,7 +77,7 @@ STORAGES = {
         "BACKEND": "storages.backends.s3.S3Storage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
